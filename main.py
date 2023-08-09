e5af2781c01a329869fb19429d6fca085a2c7335
amaheshwari01 <acoding2007@gmail.com>
2023-08-09 12:19:25 -0700

    .

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import requests
 import json
 from parse_data import parse
@@ -17,10 +18,57 @@ def get_data():
     return data
 
 
+def date_picker():
+    custom_date = input("Do you want a custom date? (yes/no): ")
+    if custom_date.lower() != "yes":
+        return (0, 0)
+
+    now = datetime.now()
+
+    year = input(f"Enter the start year (default is {now.year}): ")
+    year = int(year) if year else now.year
+
+    month = input(f"Enter the start month (default is {now.month}): ")
+    month = int(month) if month else now.month
+
+    day = input(f"Enter the start day (default is {now.day}): ")
+    day = int(day) if day else now.day
+
+    hour = input(f"Enter the start hour (default is {now.hour}): ")
+    hour = int(hour) if hour else now.hour
+
+    start_date = datetime(year, month, day, hour)
+    start_date_epoch = int(start_date.timestamp())
+
+    custom_end_date = input("Do you want a custom end date? (yes/no): ")
+    if custom_end_date.lower() == "yes":
+        year = input(f"Enter the end year (default is {now.year}): ")
+        year = int(year) if year else now.year
+
+        month = input(f"Enter the end month (default is {now.month}): ")
+        month = int(month) if month else now.month
+
+        day = input(f"Enter the end day (default is {now.day}): ")
+        day = int(day) if day else now.day
+
+        hour = input(f"Enter the end hour (default is {now.hour}): ")
+        hour = int(hour) if hour else now.hour
+
+        end_date = datetime(year, month, day, hour)
+    else:
+        end_date = now
+
+    end_date_epoch = int(end_date.timestamp())
+
+    return (start_date_epoch, end_date_epoch)
+
+
+
 if __name__ == "__main__":
     # Get the data:
     data = get_data()
     # Parse the data:
-    data = parse(data, 0, 0)
+    s,e = date_picker()
+    data = parse(data, s,e)
     # Plot the data:
     plot(data)
